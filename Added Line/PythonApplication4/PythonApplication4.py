2b003f2c71127ef5ce592bf128b93520c5ccde96
JimS20 <jimstam@hotmail.com>
2017-01-18 13:00:00 +0100

    Added line function

--- Added Line/PythonApplication4/PythonApplication4.py
+++ Added Line/PythonApplication4/PythonApplication4.py
@@ -1,8 +1,5 @@
-"""Euromaster game
-COPYRIGHT 2017, groep 6 beste"""
 
 import pygame
-import math
 
 red = (255,0,0)
 
@@ -22,45 +19,55 @@ class Game:
         #Set up the default font
         self.font = pygame.font.Font(None,30)
 
+        #Create Line
+        self.Line = Lines()
+
     #Update game logic
-    def update(self):
+   # def update(self):
         #Update entities
-        self.player.update()
+    #    self.player.update()
+
     #Draw everything
     def draw(self):
         #Clear the screen
         self.screen.fill((0, 0, 0))
 
         #Draw the lines
-        draw_line()
+        self.Line.draw(self.screen)
 
         #Draw the score text
-        self.score_text = self.font.render("Score: {}".format(self.score),1,(255, 255, 255))
-        self.screen.blit(self.score_text,(16,16))
+        #self.score_text = self.font.render("Score: {}".format(self.score),1,(255, 255, 255))
+       # self.screen.blit(self.score_text,(16,16))
 
         #Flip the screen
         pygame.display.flip()
 
-    def update_score(self):
-        self.score += 1
-     #The game loop
+    #def update_score(self):
+        #self.score += 1
+    #The game loop
     def game_loop(self):
         while not process_events():
-            self.update()
+           # self.update()
             self.draw()
 
 class Player:
-    def __init__(self, posX, posY):
-        self.score = 0
-        self.posX = posX
-        self.posY = posY
+   def __init__(self, posX, posY):
+       self.score = 0
+       self.posX = posX
+       self.posY = posY
 
-    def update(self):
+   def update(self):
         if answer == True:
-            self.score += 1
-            self.posY += 1
+          self.score += 1
+          self.posY += 1
         #elif #Hoe gaan we naar volgende speler?
     #toevoegen draw functie hier
+
+class Lines:
+    def draw(self,screen):
+        pygame.draw.line(screen,red,(60,60),(120,60),4) 
+    
+        
 
 #Handle pygame events
 def process_events():
@@ -70,23 +77,10 @@ def process_events():
             return True
     return False
 
-def draw_line():
-    pygame.draw.line(screen, red, (60,60), (120, 60),4)  
-
 #Main program logic
 def program():
     game=Game()
-     
-
-            
-           
-           
-            
-
-
-        
-            
-         
-        
-
+    game.game_loop()
+ 
+program()
 
